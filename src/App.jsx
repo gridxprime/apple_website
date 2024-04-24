e89deadd9fc7e5de114920a5fe0fcde862b975ae
Github <till_morning_comes@mail.ru>
2024-04-24 18:10:59 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Highlights from "./components/Highlights"
+import Model from "./components/Model"
+
+
+const App =() => {
+ 
+
+  return (
+    <main className="bg-black">
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <Highlights></Highlights>
+      <Model></Model>
+    </main>
+  )
+}
+
+export default App
